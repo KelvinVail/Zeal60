66b6418801576de1ad36eb5dfa779eef4b6738d0
Vail, Kelvin <VailK@lloyds.com>
2018-06-25 17:04:18 +0100

    minimal set up of layer2

--- zeal60_keymap_kelvin_pok3r_ansi.bat
+++ zeal60_keymap_kelvin_pok3r_ansi.bat
@@ -16,12 +16,12 @@ color_1=0,100,100 ^
 color_2=180,100,100 ^
 caps_lock_indicator_color=0,0,100 ^
 caps_lock_indicator_row_col=none ^
-layer_1_indicator_color=0,0,100 ^
-layer_1_indicator_row_col=none ^
-layer_2_indicator_color=0,0,100 ^
-layer_2_indicator_row_col=none ^
-layer_3_indicator_color=0,0,100 ^
-layer_3_indicator_row_col=none
+layer_1_indicator_color=10,86,92 ^
+layer_1_indicator_row_col=all ^
+layer_2_indicator_color=83,63,76 ^
+layer_2_indicator_row_col=all ^
+layer_3_indicator_color=197,86,72 ^
+layer_3_indicator_row_col=all
 
 zeal60 backlight_config_set_alphas_mods ^
 MOD      ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    MOD     ^
@@ -32,8 +32,8 @@ MOD      MOD      MOD      ALPHA    ALPHA    ALPHA    ALPHA    ALPHA    ALPHA   
 
 zeal60 keymap 0 ^
 KC_ESC   KC_1     KC_2     KC_3     KC_4     KC_5     KC_6     KC_7     KC_8     KC_9     KC_0     KC_MINS  KC_EQL   KC_BSPC ^
-KC_TAB   KC_Q     KC_W     KC_E     KC_R     KC_T     KC_Y     KC_U     KC_I     KC_O     KC_P     KC_LBRC  KC_RBRC  KC_BSLS ^
-KC_CAPS  KC_A     KC_S     KC_D     KC_F     KC_G     KC_H     KC_J     KC_K     KC_L     KC_SCLN  KC_QUOT  KC_ENT   KC_NO   ^
+KC_TAB   KC_Q     KC_W     KC_E     KC_R     KC_T     KC_Y     KC_U     KC_I     KC_O     KC_P     KC_LBRC  KC_RBRC  KC_NUBS ^
+FN_MO23  KC_A     KC_S     KC_D     KC_F     KC_G     KC_H     KC_J     KC_K     KC_L     KC_SCLN  KC_QUOT  KC_ENT   KC_NO   ^
 KC_LSFT  KC_NO    KC_Z     KC_X     KC_C     KC_V     KC_B     KC_N     KC_M     KC_COMM  KC_DOT   KC_SLSH  KC_RSFT  KC_NO   ^
 KC_LCTL  KC_LGUI  KC_LALT  KC_NO    KC_NO    KC_NO    KC_NO    KC_SPC   KC_NO    KC_NO    KC_RALT  FN_MO13  FN_MO23  KC_RCTL
 
@@ -45,9 +45,9 @@ KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_VOLD  KC_VOLU 
 KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS
 
 zeal60 keymap 2 ^
-KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS ^
-KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS ^
-KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS ^
+LGUI(KC_L)  KC_F1    KC_F2    KC_F3    KC_F4    KC_F5    KC_F6    KC_F7    KC_F8    KC_F9    KC_F10   KC_F11   KC_F12  KC_DEL  ^
+KC_TRNS  KC_TRNS  KC_TRNS  KC_EXSEL KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_BSLS ^
+KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_LEFT  KC_DOWN  KC_UP    KC_RGHT  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS ^
 KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS ^
 KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS  KC_TRNS
 
